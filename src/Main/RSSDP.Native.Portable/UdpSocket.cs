0dd8b23e6226be56feb46759754e671bda7c3065
Teemu Tapanila <teemu@tapanila.net>
2016-08-30 23:56:05 +0200

    Renamed all assemblies to rssdp.native and created a stub portable version

---
+++ src/Main/RSSDP.Native.Portable/UdpSocket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rssdp.Infrastructure;
+
+namespace Rssdp
+{
+    // THIS IS A STUB FILE
+
+    internal sealed class UdpSocket : DisposableManagedObjectBase, IUdpSocket
+    {
+        public System.Threading.Tasks.Task<ReceivedUdpData> ReceiveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendTo(byte[] messageData, UdpEndPoint endPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
