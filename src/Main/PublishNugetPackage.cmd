39037769d22fcc6585c1f791c12a3375b4b44d13
Troy Willmot <yort@yortondotnet.com>
2017-01-11 15:07:56 +1300

    Package fixes for UWP and WPA8.1 to ensure correct assembly sets referenced.

--- src/Main/PublishNugetPackage.cmd
+++ src/Main/PublishNugetPackage.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Press any key to publish
 pause
-".nuget\NuGet.exe" push Rssdp.2.0.8.nupkg
+".nuget\NuGet.exe" push Rssdp.2.0.9.nupkg
 pause
--- src/Main/Shared/AssemblyInfoCommon.cs
+++ src/Main/Shared/AssemblyInfoCommon.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCopyright("Released under the MIT license; http://choosealicense.com/licenses/mit/; https://github.com/Yortw/RSSDP")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("2.0.10.0")]
-[assembly: AssemblyFileVersion("2.0.10.0")]
+[assembly: AssemblyVersion("2.0.9.0")]
+[assembly: AssemblyFileVersion("2.0.9.0")]
