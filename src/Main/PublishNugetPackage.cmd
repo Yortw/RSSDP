9167bed13e60139dcc0f786c3a5b24989397cd00
Troy Willmot <yort@yortondotnet.com>
2017-02-26 15:22:22 +1300

    Add source command to publish bat file

--- src/Main/PublishNugetPackage.cmd
+++ src/Main/PublishNugetPackage.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Press any key to publish
 pause
-".nuget\NuGet.exe" push Rssdp.2.0.9.nupkg
+".nuget\NuGet.exe" push Rssdp.3.0.0.nupkg -Source https://www.nuget.org/api/v2/package
 pause
