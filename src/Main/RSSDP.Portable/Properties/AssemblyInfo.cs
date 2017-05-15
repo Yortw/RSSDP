281d44b6234e0935fb86f23466940ed736bb2560
Troy Willmot <yort@yortondotnet.com>
2017-05-15 23:05:52 +1200

    Update nuspec to better support WinRT. Add package test projects. Change assembly description for portable lib.

--- src/Main/RSSDP.Portable/Properties/AssemblyInfo.cs
+++ src/Main/RSSDP.Portable/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RSSDP.Native.Portable")]
+[assembly: AssemblyTitle("RSSDP.Portable")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
---
+++ src/RssdpPackageTests/RssdpPackageTests/RssdpPackageTests/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RssdpPackageTests
+{
+	public class App : Application
+	{
+		public App()
+		{
+			var listView = new ListView();
+
+			// The root page of your application
+			var content = new ContentPage
+			{
+				Title = "RssdpPackageTests",
+				Content = listView
+			};
+
+			MainPage = new NavigationPage(content);
+
+			var t = SearchAsync(listView);
+		}
+
+		private async Task SearchAsync(ListView listView)
+		{
+			var items = new List<string>();
+			var searcher = new RssdpPackageTests.DeviceSearcher();
+			foreach (var device in await searcher.SearchAsync())
+			{
+				items.Add(device.DescriptionLocation.ToString());
+			}
+			listView.ItemsSource = items;
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
