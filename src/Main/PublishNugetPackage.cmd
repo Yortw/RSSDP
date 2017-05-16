2852b3e1a6bfed1bdab9f2037ab0cdff23e7a5e6
Troy Willmot <yort@yortondotnet.com>
2017-05-16 18:18:35 +1200

    Update command file for publishing nuget.

--- src/Main/PublishNugetPackage.cmd
+++ src/Main/PublishNugetPackage.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Press any key to publish
 pause
-".nuget\NuGet.exe" push Rssdp.3.5.0-beta.nupkg -Source https://www.nuget.org/api/v2/package
+".nuget\NuGet.exe" push Rssdp.3.5.3.nupkg -Source https://www.nuget.org/api/v2/package
 pause
