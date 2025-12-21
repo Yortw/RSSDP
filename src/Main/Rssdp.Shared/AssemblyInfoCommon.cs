fb8f934869d8d88004e0f1b374d32bf8a7436923
Troy Willmot <6725891+Yortw@users.noreply.github.com>
2025-12-21 16:36:48 +1300

    Remove some more unused projects/folders.

--- src/Main/Rssdp.Shared/AssemblyInfoCommon.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyProduct("Really Simple Service Discovery Protocol")]
-[assembly: AssemblyCompany("Created by Yort. https://github.com/Yortw/RSSDP")]
-[assembly: AssemblyCopyright("Released under the MIT license; http://choosealicense.com/licenses/mit/; https://github.com/Yortw/RSSDP")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyVersion("4.0.4.0")]
-[assembly: AssemblyFileVersion("4.0.4.0")]
