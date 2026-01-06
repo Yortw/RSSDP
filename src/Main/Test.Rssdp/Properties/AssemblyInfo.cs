b9c11f0b65a189759f675d457fab4c1909327185
Troy Willmot <6725891+Yortw@users.noreply.github.com>
2026-01-06 22:23:15 +1300

    Try disabling parallel tests.

--- src/Main/Test.Rssdp/Properties/AssemblyInfo.cs
+++ src/Main/Test.Rssdp/Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Microsoft.VisualStudio.TestTools.UnitTesting.Parallelize(Workers = 1, Scope = Microsoft.VisualStudio.TestTools.UnitTesting.ExecutionScope.MethodLevel)]
